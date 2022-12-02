d92264430b9da9deba63c01e768c9b4773fc9314
Johan Planchon <dev@planchon.dev>
2022-12-02 02:34:33 +0100

    feat: added rust project files

---
+++ services/log/src/lib.rs
@@ -0,0 +1,55 @@
+#![forbid(unsafe_code)]
+#[cfg(feature = "either")]
+use either::{for_both, Either};
+#[cfg(feature = "log_file")]
+use tracing_appender::non_blocking::WorkerGuard;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+/// Re-export tracing for convenience.
+pub use tracing;
+
+/// Re-export log macros for convenience.
+pub use tracing::{debug, error, info, trace, warn};
+
+/// Re-export levels for convenience.
+pub use tracing::log::{Level, LevelFilter};
+
+pub mod layers;
+
+pub fn setup_simple_logger() {
+    // This will print tracing events to standard output for humans to read
+    tracing_subscriber::Registry::default()
+        .with(layers::with_env())
+        .with(layers::with_pretty())
+        .init();
+}
+
+#[cfg(feature = "tree")]
+pub fn setup_hierarchical_logger() {
+    // This will print tracing events to standard output for humans to read
+    tracing_subscriber::Registry::default()
+        .with(layers::with_env())
+        .with(layers::with_hierarchical())
+        .init();
+}
+
+#[cfg(all(feature = "log_file", feature = "tree"))]
+pub fn setup_logger_with_logfiles(logfile_prefix: &str) -> Option<WorkerGuard> {
+    // This will print tracing events to standard output for humans to read
+    let logger = tracing_subscriber::Registry::default()
+        .with(layers::with_env())
+        .with(layers::with_hierarchical());
+    // When this variable goes out of scope (at the end of the function where this function is called), it will flush the log file writer
+    let mut file_logger_guard = Option::None;
+
+    // Masking the inner type using "dyn" keyword because return types are differents in the if / else
+    let logger = if let Some((layer, guard)) = layers::with_logfiles(logfile_prefix) {
+        file_logger_guard = Some(guard);
+        Either::Left(logger.with(layer))
+    } else {
+        Either::Right(logger)
+    };
+
+    for_both!(logger, logger => logger.init());
+    file_logger_guard
+}
